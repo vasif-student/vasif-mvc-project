1b437ac658b5e48588666f6003b86acd068d087e
vasif-student <vasifah@code.edu.az>
2021-11-16 16:18:43 +0400

    mvc-proj

---
+++ my-mvc-project/Controllers/PlacesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_mvc_project.Controllers
+{
+    public class PlacesController : Controller
+    {
+    }
+}
